842ddd4e28a394bfae4b871e339a973712bfc8c8
vinovadi <v.vinokhodov@gmail.com>
2019-08-18 11:51:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/calendarview/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.calendarview;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.GregorianCalendar;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText txtTaskName;
+    private Button mChooseStartDate;
+    private Button mChooseEndDate;
+    private CalendarView mStartDateCalendar;
+    private CalendarView mEndDateCalendar;
+    private Button mBtnAdd;
+    private long mStartDate;
+    private String mStartDateTxt;
+    private long mEndDate;
+    private String mEndDateTxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        txtTaskName = findViewById(R.id.txtTaskName);
+        mChooseStartDate = findViewById(R.id.btnStartDate);
+        mChooseEndDate = findViewById(R.id.btnEndDate);
+        mStartDateCalendar = findViewById(R.id.cVstartDate);
+        mEndDateCalendar = findViewById(R.id.cVendDate);
+        mBtnAdd = findViewById(R.id.btnAdd);
+        final Intent intent = new Intent(MainActivity.this, MainActivity.class);
+
+        // Скроем календари при запуске приложения
+        mStartDateCalendar.setVisibility(View.GONE);
+        mEndDateCalendar.setVisibility(View.GONE);
+
+        mChooseStartDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mStartDateCalendar.setVisibility(View.VISIBLE);
+                mEndDateCalendar.setVisibility(View.GONE);
+            }
+        });
+
+        mChooseEndDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEndDateCalendar.setVisibility(View.VISIBLE);
+                mStartDateCalendar.setVisibility(View.GONE);
+            }
+        });
+
+        mStartDateCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                mStartDateTxt = i+"-"+i1+"-"+i2;
+                mChooseStartDate.setText("Дата начала задачи" + " " + mStartDateTxt);
+                GregorianCalendar gregorianCalendar = new GregorianCalendar();
+                gregorianCalendar.set(i, i1, i2);
+                mStartDate = gregorianCalendar.getTimeInMillis();
+                calendarView.setVisibility(View.GONE);
+            }
+        });
+
+        mEndDateCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                mEndDateTxt = i+"-"+i1+"-"+i2;
+                mChooseEndDate.setText("Дата окончания задачи" + " " + mEndDateTxt);
+                GregorianCalendar gregorianCalendar = new GregorianCalendar();
+                gregorianCalendar.set(i, i1, i2);
+                mEndDate = gregorianCalendar.getTimeInMillis();
+                calendarView.setVisibility(View.GONE);
+            }
+        });
+
+        mBtnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mStartDate > mEndDate){
+                    Toast.makeText(MainActivity.this, "Ошибка", Toast.LENGTH_LONG).show();
+                    mChooseStartDate.setText("Дата начала задачи");
+                    mChooseEndDate.setText("Дата завершения задачи");
+                } else {
+                    startActivity(intent);
+                    Toast.makeText(MainActivity.this, "Задача " + "'"+ txtTaskName.getText().toString() + "' успешно добавлена!" + " Старт: " + mStartDateTxt + " Завершение: " + mEndDateTxt, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+}
